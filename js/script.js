dd5d56020f453eee38a979f62e761006df8e7193
julien jamet <jamet.julien@gmail.com>
2016-01-27 10:51:01 +0100

    Add directive on process

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var aData =[
   {
-    "titre": "Andrews Byrd",
+    "titre"  : "Andrews",
+    "auteur" : "Byrd",
     "content": "Reprehenderit tempor fugiat occaecat culpa laboris voluptate occaecat ad. Commodo consectetur tempor id irure amet veniam nulla adipisicing quis esse esse adipisicing tempor. Excepteur ad laboris aute et eiusmod aliqua est laborum laborum id aute. Excepteur laboris veniam aliqua occaecat laborum deserunt ut est irure aute dolore quis nostrud laborum. Eiusmod est sit duis duis minim laborum. Reprehenderit irure laborum do deserunt adipisicing duis enim nulla proident.\r\n",
     "stat": {
       "like": 35,
@@ -8,7 +9,8 @@ var aData =[
     }
   },
   {
-    "titre": "Gamble Ball",
+    "titre"  : "Gamble",
+    "auteur" : "Ball",
     "content": "Est proident amet minim nulla ut irure irure anim labore adipisicing velit officia. Officia amet magna pariatur duis laboris do. Aliquip in minim dolore exercitation dolor incididunt commodo pariatur. Ea nisi labore minim Lorem mollit nisi occaecat cillum Lorem veniam nisi occaecat. Dolore ea adipisicing fugiat ex exercitation nostrud ut ullamco in adipisicing ex mollit velit ut. Excepteur veniam ea velit voluptate cupidatat officia veniam commodo non.\r\n",
     "stat": {
       "like": 35,
@@ -16,7 +18,8 @@ var aData =[
     }
   },
   {
-    "titre": "Farrell Kerr",
+    "titre"  : "Farrell",
+    "auteur" : "Kerr",
     "content": "Nulla do fugiat ea aute proident officia culpa pariatur consequat aute fugiat officia. Sint ut nisi laboris nulla non deserunt reprehenderit amet excepteur ad. Pariatur culpa anim veniam Lorem non. Dolore deserunt tempor ut deserunt cillum ullamco reprehenderit commodo non exercitation. Ullamco sunt laboris magna irure reprehenderit. Nisi laboris nulla occaecat adipisicing id voluptate Lorem ex sit.\r\n",
     "stat": {
       "like": 39,
@@ -24,7 +27,8 @@ var aData =[
     }
   },
   {
-    "titre": "Maynard Lambert",
+    "titre"  : "Maynard",
+    "auteur" : "Lambert",
     "content": "Sint aliqua sint ea nisi ullamco voluptate cillum. Exercitation ullamco cillum proident minim ut. Quis pariatur aliquip sint eu. Et et eiusmod non magna est officia culpa Lorem pariatur anim mollit occaecat labore. Labore Lorem aliqua velit consequat nisi proident. Incididunt id esse excepteur quis culpa commodo ullamco exercitation.\r\n",
     "stat": {
       "like": 21,
@@ -32,7 +36,8 @@ var aData =[
     }
   },
   {
-    "titre": "Carrillo Colon",
+    "titre"  : "Carrillo",
+    "auteur" : "Colon",
     "content": "Veniam nulla ex ullamco aliqua minim. Exercitation labore esse occaecat sit occaecat dolor nostrud aute. Nulla adipisicing est esse in magna aliquip voluptate cillum nulla adipisicing duis ut.\r\n",
     "stat": {
       "like": 22,
@@ -40,7 +45,8 @@ var aData =[
     }
   },
   {
-    "titre": "Martha Benjamin",
+    "titre"  : "Martha",
+    "auteur" : "Benjamin",
     "content": "Anim pariatur deserunt consectetur cillum excepteur occaecat pariatur ea in ad. Consequat tempor reprehenderit irure sit Lorem minim minim velit. Proident ut non deserunt sit aliquip. Do tempor sint ipsum quis elit fugiat mollit tempor Lorem aliquip anim occaecat. Lorem aute proident ipsum cillum sint do proident. Magna excepteur ullamco non dolore officia aliquip excepteur laborum nisi anim nisi.\r\n",
     "stat": {
       "like": 24,
@@ -48,7 +54,8 @@ var aData =[
     }
   },
   {
-    "titre": "Chandler Wolf",
+    "titre"  : "Chandler",
+    "auteur" : "Wolf",
     "content": "Labore cillum reprehenderit veniam ut irure deserunt tempor ut occaecat veniam. Do irure consectetur cillum commodo velit Lorem excepteur mollit elit adipisicing ea cillum. Id commodo id aliqua duis duis. Eiusmod enim commodo pariatur veniam quis occaecat ut. Anim ipsum duis esse eiusmod nostrud sit ea magna veniam aute non fugiat cillum anim. Sint id minim commodo ex ex aliquip exercitation anim culpa laborum proident duis exercitation.\r\n",
     "stat": {
       "like": 34,
@@ -56,7 +63,8 @@ var aData =[
     }
   },
   {
-    "titre": "Kristie Figueroa",
+    "titre"  : "Kristie",
+    "auteur" : "Figueroa",
     "content": "Ex nisi laborum fugiat aute deserunt ad. Laboris mollit esse elit ea ut aute excepteur ut do incididunt veniam cillum aliquip reprehenderit. Amet veniam sint enim Lorem ad ullamco laboris excepteur. Qui occaecat quis deserunt dolore ullamco nisi reprehenderit.\r\n",
     "stat": {
       "like": 38,
@@ -64,7 +72,8 @@ var aData =[
     }
   },
   {
-    "titre": "Rosario Fry",
+    "titre"  : "Rosario",
+    "auteur" : "Fry",
     "content": "Magna amet nisi voluptate cupidatat anim magna consequat sit. Ad voluptate anim commodo tempor cupidatat consectetur exercitation non eiusmod culpa elit velit in officia. Deserunt voluptate voluptate cillum fugiat pariatur officia eiusmod id nostrud nostrud ut.\r\n",
     "stat": {
       "like": 37,
@@ -72,7 +81,8 @@ var aData =[
     }
   },
   {
-    "titre": "Hester Chang",
+    "titre"  : "Hester",
+    "auteur" : "Chang",
     "content": "Lorem consequat pariatur sunt voluptate incididunt deserunt esse. Consequat quis cupidatat do ut consequat est ad reprehenderit cillum sint laborum. Cillum culpa id do velit velit. Elit aute aute voluptate qui quis eiusmod incididunt esse ullamco laborum do sunt anim labore. Aute consectetur labore velit magna proident laborum consectetur nostrud veniam. Do excepteur laborum occaecat in id minim commodo esse. Ut voluptate do ex exercitation occaecat tempor cillum deserunt fugiat proident deserunt laboris.\r\n",
     "stat": {
       "like": 27,
@@ -80,7 +90,8 @@ var aData =[
     }
   },
   {
-    "titre": "Skinner Reeves",
+    "titre"  : "Skinner",
+    "auteur" : "Reeves",
     "content": "Non incididunt consequat sunt consectetur consectetur labore esse sunt. Veniam enim velit eu ex do laboris exercitation dolore qui. Id anim ut ex consequat duis cupidatat enim. Ut commodo ea mollit deserunt ut dolor dolore fugiat esse. Nisi laboris consequat labore excepteur nulla aliquip elit ad velit cupidatat nisi officia do. Cupidatat enim veniam voluptate aliquip veniam sit in excepteur nostrud.\r\n",
     "stat": {
       "like": 37,
@@ -88,7 +99,8 @@ var aData =[
     }
   },
   {
-    "titre": "Valentine Ortega",
+    "titre"  : "Valentine",
+    "auteur" : "Ortega",
     "content": "Nulla fugiat qui aute amet eiusmod elit velit laboris ea. Laborum veniam eu ex laboris proident. Veniam voluptate amet eu dolor amet ut dolor cillum ea voluptate ullamco minim consectetur aute. Velit ea cillum dolor officia ullamco veniam Lorem pariatur voluptate sint.\r\n",
     "stat": {
       "like": 21,
@@ -96,7 +108,8 @@ var aData =[
     }
   },
   {
-    "titre": "Walls Blackwell",
+    "titre"  : "Walls",
+    "auteur" : "Blackwell",
     "content": "Sint quis eiusmod officia consectetur ea sunt velit. Ipsum fugiat incididunt commodo sunt id nulla pariatur ad deserunt nisi ullamco sint commodo dolor. Elit incididunt et nostrud ut exercitation elit et labore sint magna velit Lorem Lorem irure. Ipsum anim laborum elit laborum fugiat cupidatat. Fugiat esse labore anim excepteur id eiusmod quis tempor veniam ipsum. Elit voluptate incididunt enim ex duis ipsum.\r\n",
     "stat": {
       "like": 25,
@@ -104,7 +117,8 @@ var aData =[
     }
   },
   {
-    "titre": "Bridget Odonnell",
+    "titre"  : "Bridget",
+    "auteur" : "Odonnell",
     "content": "Elit sunt et cillum magna consequat anim minim. Aliquip quis consectetur ut minim dolor ea fugiat excepteur excepteur consectetur. Est adipisicing adipisicing sit veniam. Laborum dolor eu laboris voluptate et. Laboris nisi fugiat laboris qui ea non proident Lorem labore laborum nisi. Sunt magna eu ex ipsum amet ut exercitation ullamco irure id qui.\r\n",
     "stat": {
       "like": 22,
@@ -112,7 +126,8 @@ var aData =[
     }
   },
   {
-    "titre": "Liz Guy",
+    "titre"  : "Liz ",
+    "auteur" : "uy",
     "content": "Lorem enim id officia veniam fugiat fugiat minim culpa id adipisicing cillum deserunt do culpa. Duis cupidatat magna ea excepteur dolore excepteur sint fugiat. Laboris proident amet tempor ex qui proident enim eu.\r\n",
     "stat": {
       "like": 29,
@@ -120,7 +135,8 @@ var aData =[
     }
   },
   {
-    "titre": "Valenzuela Morton",
+    "titre"  : "Valenzuela",
+    "auteur" : "Morton",
     "content": "Adipisicing anim voluptate eu minim ad occaecat labore ipsum. Tempor id eu ad nisi aliqua labore officia ipsum quis esse. Consectetur consectetur officia ex pariatur id minim eu.\r\n",
     "stat": {
       "like": 36,
@@ -128,7 +144,8 @@ var aData =[
     }
   },
   {
-    "titre": "Nola Calderon",
+    "titre"  : "Nola",
+    "auteur" : "Calderon",
     "content": "Elit laboris nostrud veniam consequat laboris incididunt minim ut ea veniam in nulla. Laboris nulla velit do non in consectetur velit do deserunt fugiat. Incididunt amet elit occaecat eu aute est proident. Amet exercitation consectetur fugiat officia proident reprehenderit in enim esse adipisicing nulla incididunt labore consequat. Culpa esse amet excepteur do reprehenderit elit deserunt enim aliqua deserunt Lorem. Laborum et eiusmod minim laboris irure cupidatat commodo non quis est dolor officia.\r\n",
     "stat": {
       "like": 21,
@@ -137,8 +154,11 @@ var aData =[
   }
 ];
 
-
-
+oDirective = {
+  auteur : function( sValue){
+    return this.titre + " by " + sValue;
+  }
+};
 /**
  * [onload description]
  * @return {[type]} [description]
@@ -158,7 +178,7 @@ window.onload = function(){
     for( i = 0; i<aData.length; i++){
 
         var oData   = aData[i];
-        var sResult = oTemplate.compute( oData);
+        var sResult = oTemplate.compute( oData, oDirective);
 
         oWrapper.innerHTML += sResult;
 
--- js/template.class.js
+++ js/template.class.js
@@ -1,3 +1,27 @@
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
+
 /**
  * [Template description]
  * @param {[type]} sUrlScript [description]
@@ -41,17 +65,30 @@ Template.prototype.treatObjet = function( sPrefix, oItem, oParent) {
  * @param  {[type]} oItem [description]
  * @return {[type]}       [description]
  */
-Template.prototype.compute = function( _oItem) {
+Template.prototype.compute = function( _oItem, _oDirective) {
 
-    var oItem     = JSON.parse( JSON.stringify( _oItem));
+    _oDirective = _oDirective || {};
 
-    var sTemplate = this._sTemplate;
+    var oItem      = JSON.parse( JSON.stringify( _oItem)),
+        oDirective = Object.assign({}, _oDirective),
+        sTemplate  = this._sTemplate,
+        sValue     = '';
 
     this.treatObjet( '', oItem, {});
+    this.treatObjet( '', oDirective, {});
 
     for (var sAttr in oItem) {
 
-        sTemplate = sTemplate.split( this._aPattern[0] + sAttr.toUpperCase() + this._aPattern[1]).join( oItem[sAttr]);
+        if( oDirective[ sAttr] && (typeof oDirective[sAttr] == 'function')){
+
+          sValue = oDirective[sAttr].call( _oItem, oItem[sAttr]);
+
+        }else{
+
+          sValue = oItem[sAttr];
+        }
+
+        sTemplate = sTemplate.split( this._aPattern[0] + sAttr.toUpperCase() + this._aPattern[1]).join( sValue);
 
     }
 
